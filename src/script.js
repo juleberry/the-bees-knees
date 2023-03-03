0ee6a351f4a85a0cfcf4f0d180bd8a9e801948fb
juleberry <105573843+juleberry@users.noreply.github.com>
2023-03-03 13:34:07 -0500

    add close menu on ESC functionality

--- src/script.js
+++ src/script.js
@@ -6,3 +6,21 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
+
+function closeNav() {
+  navList.classList.remove('show')
+}
+
+hamburgerButton.addEventListener('keyup', function(e) {
+  if (e.key == "Escape") {
+    closeNav();
+  }
+});
+
+
+const logo = document.getElementById('jbLogo')
+function logoFlip() {
+  $('logo').load(function() {
+    $(this).addClass("loaded");
+  });
+}
