317df83600655bc5cfb0f2f0249179d76133984b
juleberry <105573843+juleberry@users.noreply.github.com>
2023-03-17 20:50:41 -0400

    add minor animation to icons

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+//-----------------------
+// animate hamburger menu
+//------------------------
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 
@@ -16,3 +19,60 @@ hamburgerButton.addEventListener('keyup', function(e) {
     closeNav();
   }
 });
+
+//---------------
+// animate icons
+//---------------
+
+const reactIcon = document.getElementById('icon-react')
+const jsIcon = document.getElementById('icon-js')
+const cssIcon = document.getElementById('icon-css3')
+const htmlIcon = document.getElementById('icon-html5')
+
+function animIcon1() {
+  reactIcon.classList.add('iconHover')
+}
+
+function animIcon2() {
+  jsIcon.classList.add('iconHover')
+}
+
+function animIcon3() {
+  cssIcon.classList.add('iconHover')
+}
+
+function animIcon4() {
+  htmlIcon.classList.add('iconHover')
+}
+
+reactIcon.addEventListener('mouseover', animIcon1);
+
+jsIcon.addEventListener('mouseover', animIcon2);
+
+cssIcon.addEventListener('mouseover', animIcon3);
+
+htmlIcon.addEventListener('mouseover', animIcon4);
+
+function animReset1() {
+  reactIcon.classList.remove('iconHover')
+}
+
+function animReset2() {
+  jsIcon.classList.remove('iconHover')
+}
+
+function animReset3() {
+  cssIcon.classList.remove('iconHover')
+}
+
+function animReset4() {
+  htmlIcon.classList.remove('iconHover')
+}
+
+reactIcon.addEventListener('mouseout', animReset1);
+
+jsIcon.addEventListener('mouseout', animReset2);
+
+cssIcon.addEventListener('mouseout', animReset3);
+
+htmlIcon.addEventListener('mouseout', animReset4);
